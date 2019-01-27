66fc6af884a49d7c26e355f0936170022b90b05b
Alex Supkay <asupkay1124@gmail.com>
2019-01-27 15:32:37 -0500

    Fix bug with checking if triangle

--- HW 01/classifyTriangle.py
+++ HW 01/classifyTriangle.py
@@ -2,7 +2,7 @@ import math
 import pytest
 
 def classifyTriangle(a, b, c):
-    if(a + b <= c and a + c <= b and b + c <= a):
+    if(a + b <= c or a + c <= b or b + c <= a):
         return 'NotATriangle'
     if(a == b and a == c):
         return 'Equilateral'
@@ -21,7 +21,7 @@ def classifyTriangle(a, b, c):
 def runClassifyTriangle(a, b, c):
     print('classifyTriangle(' + str(a) + ',' + str(b) + ',' + str(c) + ')=' + classifyTriangle(a,b,c)) 
 
-class TestClass(object):
+class TestTriangles(object):
     def test_Right(self):
         assert classifyTriangle(3,4,5) == 'Right'
 
@@ -30,7 +30,10 @@ class TestClass(object):
 
     def test_Isoceles(self):
         assert classifyTriangle(10,10,10) != 'Isoceles'
+        assert classifyTriangle(5, 5, 3) == 'Isoceles'
 
     def test_Scalene(self):
-        assert classifyTriangle(10,15,30) == 'Scalene'
+        assert classifyTriangle(13,9,14) == 'Scalene'
 
+    def test_NotATriangle(self):
+        assert classifyTriangle(100, 1, 1) == 'NotATriangle'
